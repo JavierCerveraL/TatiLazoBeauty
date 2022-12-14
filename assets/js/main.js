d4c38d352ed6e04be6b5f944bf19ade5d5d07eb8
Javier Cervera <jvrmgmt@gmail.com>
2022-12-13 23:13:24 -0500

    shop with modal, not finished

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("click", function(e){
+    if(e.target.classList.contains("productos-item")){
+        const src = e.target.getAttribute("src");
+    
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById("products-modal"))
+        myModal.show();
+    }
+})

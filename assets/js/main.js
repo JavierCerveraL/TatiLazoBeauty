e203255461ba87615bd2692285e9c4972255f6be
Javier Cervera <jvrmgmt@gmail.com>
2023-01-07 14:29:10 -0500

    new policy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,31 @@
-
 // MODAL ///
 
-document.addEventListener("click", function(e){
-    if(e.target.classList.contains("productos-item")){
-        const src = e.target.getAttribute("src");
-    
-        document.querySelector(".modal-img").src = src;
-        const myModal = new bootstrap.Modal(document.getElementById("products-modal"))
-        myModal.show();
-    }
-})
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('productos-item')) {
+    const src = e.target.getAttribute('src');
 
-
-
+    document.querySelector('.modal-img').src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById('products-modal')
+    );
+    myModal.show();
+  }
+});
 
 // MODAL ENDS//
 
-
-
 // POPUP //
 
-window.addEventListener("load", function(){
-    this.setTimeout(
-        function open(event) {
-            document.querySelector(".popup").style.display = "block";
-        },
-        700
-    )
-})
-
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none"
+window.addEventListener('load', function () {
+  this.setTimeout(function open(event) {
+    document.querySelector('.popup').style.display = 'block';
+  }, 700);
 });
 
+document.querySelector('#close').addEventListener('click', function () {
+  document.querySelector('.popup').style.display = 'none';
+});
 
 // POPUP ENDS//
+
+rfc;
